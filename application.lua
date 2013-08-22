23463832e5715c19ea9b746fbff92cf97a6c7c8e
adelgado <adelgado1313@gmail.com>
2013-08-23 01:50:20 +0200

    Update application.lua

--- application.lua
+++ application.lua
@@ -2,7 +2,7 @@ local routes = {}
 
 -- Attaches a handler to a get request on a route
 local get = function (route, handler)
-	if not routes[route] or type(routes[route]) == 'table' then
+	if not routes[route] or type(routes[route]) ~= 'table' then
 		routes[route] = {}
 	end
 

e9da061006912d35c4e5ad427bb31e532b32d0f1
Alessandro Delgado <adelgado1313@gmail.com>
2014-11-30 17:57:43 -0800

    It's cool to have uris

--- chappelle.lua
+++ chappelle.lua
@@ -1,57 +1,63 @@
-local chappelle = {}
-
 local resources = {}
 local settings  = {}
 
-ngx.say('hello world')
---- Create server and listen on a port
--- @param port The port to listen on
-function chappelle.listen(port, host)
-	http.createServer(function (req, res)
-		print(req.method, req.url)
+local function has_handler()
+	return false
+end
 
-		if has_handler(req.url, method) then
-			print('OK')
+local start = function ()
+	raw_header = ngx.req.raw_header()
+	first_line = string.match(raw_header, '^(.-)\n')
+	method = ngx.req.get_method()
+
+	matcher = method .. ' /(.-) HTTP'
+
+	url = string.match(first_line, matcher)
+
+	if url then
+		ngx.print(url)
+		if has_handler(url, method) then
+			ngx.print('OK')
 			resources[req.url][method](req, res)
 		else
-			error_404(req, res)
+			ngx.print('404')
 		end
-	end):listen(port)
-end
-
-local function make_resource(method)
-	return function(resource_match, handler)
-		if type(resources[resource_match]) ~= 'table' then
-			resources[resource_match] = {}
-		end
-
-		resources[resource_match][method] = handler
 	end
 end
 
-chappelle.get    = make_resource 'GET'
-chappelle.post   = make_resource 'POST'
-chappelle.put    = make_resource 'PUT'
-chappelle.delete = make_resource 'DELETE'
+-- local function has_handler(url, method)
+-- 	if type(resources[url])  ~= 'table' then
+-- 		return false
+-- 	end
 
-local function has_handler(url, method)
-	if type(resources[url])  ~= 'table' then
-		return false
-	end
+-- 	if type(resources[url][method]) ~= 'function' then
+-- 		return false
+-- 	end
 
-	if type(resources[url][method]) ~= 'function' then
-		return false
-	end
+-- 	return true
+-- end
 
-	return true
-end
+-- local function make_resource(method)
+-- 	return function(resource_match, handler)
+-- 		if type(resources[resource_match]) ~= 'table' then
+-- 			resources[resource_match] = {}
+-- 		end
 
-local function error_404(req, res)
-	res:writeHead(404, {
-		["Content-Type"] = "text/plain",
-		["Content-Length"] = #body
-	})
-	res:finish(body)
-end
-	
---return chappelle
+-- 		resources[resource_match][method] = handler
+-- 	end
+-- end
+
+-- chappelle.get    = make_resource 'GET'
+-- chappelle.post   = make_resource 'POST'
+-- chappelle.put    = make_resource 'PUT'
+-- chappelle.delete = make_resource 'DELETE'
+
+-- local function error_404(req, res)
+-- 	res:writeHead(404, {
+-- 		["Content-Type"] = "text/plain",
+-- 		["Content-Length"] = #body
+-- 	})
+-- 	res:finish(body)
+-- end
+
+start()
